e88b0423fcfc638751dd5950a0bd04b9a78b3306
Jesus Ontiveros <jesusontiveros003@gmail.com>
2017-08-29 16:14:25 -0600

    Se agrego validacion ara regalar sodas a la clase MainActivity

--- Practica4/app/src/main/java/com/example/root/practica4/MainActivity.java
+++ Practica4/app/src/main/java/com/example/root/practica4/MainActivity.java
@@ -11,9 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String nombre = "Jesus";
-        String apellido = "Ontiveros";
-        Toast toast = Toast.makeText(getApplicationContext(),nombre+" "+apellido,Toast.LENGTH_LONG);
-        toast.show();
+        int num = 3;
+        int precio = 40;
+        int total = num*precio;
+        if(total>80){
+            Toast toast = Toast.makeText(getApplicationContext(),"Soda gratis :D",Toast.LENGTH_LONG);
+            toast.show();
+        }else {
+            Toast toast = Toast.makeText(getApplicationContext(),"No tienes una soda gratis :(",Toast.LENGTH_LONG);
+            toast.show();
+        }
+
     }
 }
