0c83492d50c585413aecd0c3c460cfae5e05f983
root <root@localhost.localdomain>
2017-08-29 15:32:29 -0600

    Se mensaje toast a la clase MainActivity

--- Practica4/app/src/main/java/com/example/root/practica4/MainActivity.java
+++ Practica4/app/src/main/java/com/example/root/practica4/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.root.practica4;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(getApplicationContext(),"el chuy",Toast.LENGTH_LONG);
+        toast.show();
     }
 }
